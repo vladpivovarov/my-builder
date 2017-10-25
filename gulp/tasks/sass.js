8c02371012f99c2813e9b93744fc519e6c3bbe07
Vladislav Pivovarov <vladpivovarov2797@ya.ru>
2017-10-25 19:34:42 +0300

    upgrade sprite:png and rebuild structure of project

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,9 +2,11 @@
 
 module.exports = function() {
   $.gulp.task('sass', function() {
-    return $.gulp.src('./source/style/*.scss')
+    return $.gulp.src('./source/style/app.scss')
       .pipe($.gp.sourcemaps.init())
+      .pipe($.gp.sassGlob())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+      .pipe($.gp.csso())
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/css'))
--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -1,13 +1,18 @@
-"use strict"
+"use strict";
 
 module.exports = function() {
   $.gulp.task('sprite:png', function () {
-    return $.gulp.src('./source/sprite/*.png')
+    var spriteData = $.gulp.src('./source/sprite/*.png')
       .pipe($.gp.spritesmith({
                 imgName: 'sprite.png',
-                cssName: 'sprite.css'
-      }))
-      .pipe($.gulp.dest($.config.root + '/assets/img'));
+                cssName: 'sprite.scss',
+                cssFormat: "css",
+                imgPath: "../img/sprite.png",
+                padding: 80
+      }));
+      var a = spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img'));
+      var b = spriteData.css.pipe($.gulp.dest('source/style/sprite'));
+      return(a,b);
     });
 }
 
